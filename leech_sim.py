2932c3d6489ab676ae64fc8aabbce2fe0bc0f663
p4nicdrunk <salvijor@msu.edu>
2016-12-14 03:41:53 -0500

    working code, neural net model is crappy though

--- leech_sim.py
+++ leech_sim.py
@@ -10,14 +10,12 @@ import numpy.matlib as ml
 import pymunk as pm
 import matplotlib.pyplot as mpl
 
-
-def add_worm(space, worm_segs):
-	segments = 8
+def add_worm(space, worm_segs, num_segs):
 	mass = 1
 	radius = 20
 	
 	x = 100
-	for i in range(0,segments):
+	for i in range(0,num_segs):
 		moment = pymunk.moment_for_circle(mass, 0, radius)
 		body = pymunk.Body(mass, moment)
 		x = x + 40
@@ -26,7 +24,7 @@ def add_worm(space, worm_segs):
 		shape.friction = 1.0
 		resting_length = 10
 		stiffness = 20
-		damping = 5
+		damping = 2
 		if i > 0:
 			jointL = pymunk.DampedSpring(body, worm_segs[i-1][0], (0,20), (0,20), resting_length, stiffness, damping)
 			jointC = pymunk.DampedSpring(body, worm_segs[i-1][0], (-20,0), (20,0), .1, stiffness, damping)
@@ -42,11 +40,12 @@ def to_pygame(p):
     return int(p.x), int(-p.y+600)
 
 def main():
+	np.set_printoptions(threshold=np.nan)
 
 
 	################ izhikevich setup ################
 
-	segments = 8
+	segments = 7
 
 	Ne=segments*12+2                 
 	Ni=0
@@ -56,26 +55,24 @@ def main():
 	b=np.vstack([hstack([0.2*ml.ones((Ne,1))]),hstack([0.25-0.05*ri])])
 	c=np.vstack([hstack([-65+15*np.power(re,2)]),hstack([-65*ml.ones((Ni,1))])])
 	d=np.vstack([hstack([8-6*np.power(re,2)]),hstack([2*ml.ones((Ni,1))])])
+
 	S=ml.zeros((Ne+Ni,Ne+Ni))
-	v=-65*ml.ones((Ne+Ni,1))
-	u=np.multiply(b,v)
+	volt=ml.ones((Ne+Ni,1))*-65
+	u=volt
+	np.multiply(b,u)
+	
 
-
-	firings=np.matrix([])
-
-
-	for i in range(1,segments):
+	for i in range(0,segments):
 		n = 12*i
-		for ii in range(2,12):
+		for ii in range(1,12):
 			# connect each neuron to the next one in segment
-			S[(n-ii)+2,(n-ii)+1] = 40
+			S[((n-1)+ii)+1,((n-1)+ii)] = 40
 		# connect the mid neuron to the start of the next segment
-		S[n+1,n-6] = 40
+		S[n+1,n-9] = 40
 	# connect the last neuron  in the first to the first in the first segment
 	# so that it fires cyclically 
-	S[1,12] = 40
-	   
-
+	S[0,11] = 40
+	#inpu = input()
 	startI = 20
 	I = ml.zeros((Ne+Ni,1))
 	I[0] = startI
@@ -89,17 +86,25 @@ def main():
     	clock = pygame.time.Clock()
 
 	space = pymunk.Space()
-	space.gravity = (0.0, -100.0)
+	space.gravity = (0.0, -50.0)
 
-	shape = pymunk.Segment(space.static_body, (5, 100), (595,100), 1.0)
+	shape = pymunk.Segment(space.static_body, (0,0), (800,0), 1.0)
         shape.friction = 1.0
         space.add(shape)
-	
+	shape = pymunk.Segment(space.static_body, (0,0), (0,100), 1.0)
+        shape.friction = 1.0
+        space.add(shape)
+	shape = pymunk.Segment(space.static_body, (800,0), (800,100), 1.0)
+        shape.friction = 1.0
+        space.add(shape)
+
     	worm_segs = []
     	draw_options = pymunk.pygame_util.DrawOptions(screen)
-	lines = add_worm(space, worm_segs)
+	lines = add_worm(space, worm_segs, segments)
 	counter = 0
-
+		
+	lengthL = 30
+	lengthR = 30
 
     	while True:
         	for event in pygame.event.get():
@@ -108,45 +113,74 @@ def main():
             		elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 		sys.exit(0)
 
- 		############### izhikevich #################
-		fired=np.argwhere(v>=30)
-		v[fired,0]=c[fired,0]
-		u[fired,0]=u[fired,0]+d[fired,0]
-		I=I+S[:,fired].sum()
-		v=v+0.5*(0.04*np.power(v,2)+5*v+140-u+I)
-		v=v+0.5*(0.04*np.power(v,2)+5*v+140-u+I)
-		u=u+np.multiply(a,(np.multiply(b,v-u)))
-		I = ml.zeros((Ne+Ni,1))
-
 
 		########### leech #############
         	space.step(1/50.0)
 
         	screen.fill((255,255,255))
         	space.debug_draw(draw_options)
-		for i in range(1,segments):
-			'''
-			print(v[(i*6)-3,0])
-			if v[(i*6)-3,0] > 30:
-				lengthL = 0
-			else:
-				lengthL = 30
 
-			if v[(i*6)+3,0] > 30:
-				lengthR = 0
+
+ 		############### izhikevich #################
+		
+		fired=np.zeros((Ne+Ni,1))
+		for i in range(0,Ne+Ni):
+			if volt[i]>=30:
+				fired[i]=1
+				volt[i] = c[i]
+				u[i]=u[i]+d[i]
+			
+		S2 = S*fired
+		I=np.add(I,S2*2)
+		print(I)
+
+		volt=volt+0.5*(0.04*ml.power(volt,2)+5*volt+140-u+I)
+		volt=volt+0.5*(0.04*ml.power(volt,2)+5*volt+140-u+I)
+		u=u+np.multiply(a,(np.multiply(b,volt))-u)
+		I = ml.zeros((Ne+Ni,1))
+
+
+		for i in range(1,segments):
+			#'''
+
+			if fired[(i*12)-1] > 0:
+				lengthL = -200
 			else:
+				lengthL = 0
+
+			if fired[(i*12)-9] > 0:
+				lengthR = -200
+			else:
+				lengthR = 0
+
+			worm_segs[i][2].rest_length = lengthL
+			worm_segs[i][3].rest_length = lengthR
+			#'''	
+
+			'''
+			#print(volt[(i*6)-3,0])
+			if fired[(i*6)-3] > 0:
+				lengthL = 30
+			else:
+				if lengthL >= -30:
+					lengthL -= 3
+
+			if fired[(i*6)+3] > 0:
 				lengthR = 30
+			else:
+				if lengthR >= -30:
+					lengthR -= 3
 
 			worm_segs[i][2].rest_length = lengthL
 			worm_segs[i][3].rest_length = lengthR
 			'''
 		
-			#'''
+			'''
 			# sine oscillator for test
 			length = math.sin((counter+10*i)*3.14159265/45)*20
-			worm_segs[i][2].rest_length = length
-			worm_segs[i][3].rest_length = -length
-			#'''
+			worm_segs[i][2].rest_length = -length
+			worm_segs[i][3].rest_length = length
+			'''
 		counter = counter + 1
         	pygame.display.flip()
         	clock.tick(50)
